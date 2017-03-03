069b2afb24ec159636bf53193ea437c9d8abc405
Nicholas Galarza <nicogala93@gmail.com>
2017-03-03 00:52:19 -0500

    Included Tweet button

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ Basic web application to display random quotes from Andruxnet API.
 (+) Designed interface. 
 (+) Included JQuery
 (+) Makes GET call to Andrux net
-(-) Can tweet quote 
+(+) Can tweet quote 
 (-) Gear spins on button press
--- js/quotes.js
+++ js/quotes.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
+    var tQuote, tAuthor;
+
     function getHardcodedQuote() {
         var quotes = ["Software is like sex: it's better when its free.",
             "Whether gods exist or not, there is no way to get absolute certainty about ethics. Without absolute certainty, what do we do? We do the best we can.",
-            "That's what Paradise is- never knowing the difference.",
+            "That's what Paradise is - never knowing the difference.",
             "Kindness is the language which the deaf can hear and the blind can see.",
             "What transforms this world is — knowledge. Do you see what I mean? Nothing else can change anything in this world. Knowledge alone is capable of transforming the world, while at the same time leaving it exactly as it is. When you look at the world with knowledge, you realize that things are unchangeable and at the same time are constantly being transformed.",
             "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking. Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.",
@@ -13,9 +15,11 @@ $(document).ready(function() {
         ];
         var authors = ["Linus Torvalds", "Richard Stallman", "Joesph Heller", "Mark Twain", "Yukio Mishima", "Steve Jobs", "Confucius", "David Allen", "Albert Einstein"];
         var random = Math.floor(Math.random() * quotes.length);
+        tQuote = quotes[random];
+        tAuthor = authors[random];
 
-        $("#quote").hide().fadeIn(2500).text('"' + quotes[random] + '"');
-        $("#author").hide().fadeIn(2500).text('- ' + authors[random]);
+        $("#quote").hide().fadeIn(2500).text('"' + tQuote + '"');
+        $("#author").hide().fadeIn(2500).text('- ' + tAuthor);
     }
 
     $('#newQuote').click(function() {
@@ -28,8 +32,10 @@ $(document).ready(function() {
                 type: "GET",
                 dataType: 'json',
                 success: function(data) {
-                    $('#quote').hide().fadeIn(2500).text('"' + data.quote + '"');
-                    $('#author').hide().fadeIn(2500).text('- ' + data.author);
+                    tQuote = data.quote;
+                    tAuthor = data.author;
+                    $('#quote').hide().fadeIn(2500).text('"' + tQuote + '"');
+                    $('#author').hide().fadeIn(2500).text('- ' + tAuthor);
                 },
                 error: function(xhr, status, error) {
                     getHardcodedQuote();
@@ -40,5 +46,13 @@ $(document).ready(function() {
                 }
             })
         }
+    });
+
+    $('#tweet').click(function() {
+        if(tQuote.length > 140){
+          tQuote="Woops! The quote is too long for Twitter's preferences!";
+          tAuthor="The Developer Of The Random Quote Machine";
+        }
+        window.open("https://twitter.com/intent/tweet?text=" + tQuote + " -- " + tAuthor);
     });
 });
